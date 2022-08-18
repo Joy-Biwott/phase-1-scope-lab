aa816623b3b4dfd3334c8e10f8ff7f14344d1337
Joy <biwott.joy.03@gmail.com>
2022-08-18 17:24:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    return customerName= customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+    return setBestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+    return overwriteBestCustomer;
+}
+
+const leastFavoriteCustomer = 'Mike';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Janice';
+    return changeLeastFavoriteCustomer;
+}
